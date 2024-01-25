70d22f2ef84684c8915522ae0c018f83f9df9b70
Teko13 <ffabrice999@gmail.com>
2024-01-25 17:25:36 +0100

    add authentication system

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/api/login_check', name: 'app_login')]
+    public function index()
+    {
+        //
+    }
+}
